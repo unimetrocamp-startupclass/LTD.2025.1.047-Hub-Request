88ce0ef4d9ce75a688ccf96b2003414f21021333
beea15 <mrs.beatriz.colombo15@gmail.com>
2025-03-12 09:07:10 -0300

    a parte de cadastrar usuario esta funcionando, era um problema com a coluna tipos_user, porque eu exclui ela do domain mas nao foi excluido no bd.Agora eu vou para a parte de login, e pos login

--- src/main/java/com/reqhub/reqhub/service/UsuarioService.java
+++ src/main/java/com/reqhub/reqhub/service/UsuarioService.java
@@ -1,6 +1,9 @@
 package com.reqhub.reqhub.service;
 import com.reqhub.reqhub.domain.Usuario;
+
 import com.reqhub.reqhub.repository.UsuarioRepository;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
+    
+  
     public void salvarUsuario(Usuario usuario) {
         usuarioRepository.save(usuario);
     }
